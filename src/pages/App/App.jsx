a3506bb2660f86d56bcaaa5c920f579e4b8ed843
Victorszmag <Vszmaglinski@gmail.com>
2023-03-20 12:24:52 -0700

    login/setup app defult page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,48 +1,42 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import {Routes,Route,Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-//pages
-import AuthPage from "../AuthPage/AuthPage";
+// Pages
 import NewListPage from "../NewListPage/NewListPage";
-import ShowListPage from "../ShowListPage/ShowListPage"
-//components
-import {getUser} from "../..utilities/user-service";
+import ShowListPage from "../ShowListPage/ShowListPage";
+import AuthPage from "../AuthPage/AuthPage";
+// Components
+// import NavBar from "../../components/NavBar/NavBar";
+import { getUser } from "../../utilities/users-service";
 import styles from "./App.module.scss";
 
 function App() {
-    const[user,setUser] = useState(getUser());
+  const [user, setUser] = useState(getUser());
 
-    return (
-        <main className={styles.App} >
-            {user ? (
-                <>
-                    <Routes>
-                        <Route
-                            path="/lists/new"
-                            element={<NewListPage user={user} setUser={setUser}/>}
-                        />
-                        <Route
-                        path="/lists"
-                        element={<ShowListPage user={user} setUser={setUser}/>}
-                        />
-                        {/*redirect to lists/new*/}
-                        <Route path="/*" element={<Navigate to="/lists/new"/>}/>
-                        </Routes>
-                        </>
-                        ) : (
-                            <AuthPage setUser={setUser} />
-                        )}
-        </main>
-    );
- }
+  return (
+    <main className={styles.App}>
+      {user ? (
+        <>
+          {/* <NavBar /> */}
+          <Routes>
+            <Route
+              path="/lists/new"
+              element={<NewListPage user={user} setUser={setUser} />}
+            />
+            <Route
+              path="/lists"
+              element={<ShowListPage user={user} setUser={setUser} />}
+            />
+            {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
+            <Route path="/*" element={<Navigate to="/lists/new" />} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
+    </main>
+  );
+}
 
 export default App;
-
-
-
-
-
-                
-
-        
