99ad7e5c1884b1982df1cb575972e8f2340ac850
John Volpitta <johnvolpitta@gmail.com>
2023-03-22 21:45:51 -0700

    pages are rendering data

--- src/pages/NavBar/FathersDay/FathersDay.jsx
+++ src/pages/NavBar/FathersDay/FathersDay.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./FathersDay.module.scss";
+import axios from "axios";
+const BASE_URL = "http://localhost:3001/api/ideas";
 
 export function FathersDay() {
-  return <div>FathersDay</div>;
+  const [ideas, setIdeas] = useState([]);
+
+  useEffect(() => {
+    const getIdeas = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/`);
+        setIdeas(
+          response.data.filter((idea) =>
+            idea.category ? idea.category.name === "Father's Day" : [...ideas]
+          )
+        );
+        console.log(ideas);
+      } catch (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      }
+    };
+
+    getIdeas();
+  }, []);
+
+  return (
+    <div className={styles.FathersDay}>
+      <h1>Father's Day Gift Ideas</h1>
+      <ul>
+        {ideas.map((idea, _id) => (
+          <li key={_id}>
+            <h2>{idea.title}</h2>
+            <img src={idea.img} alt={idea.name} width={600} height={600} />
+            <p>{idea.description}</p>
+            <a href={idea.link}>Share</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/pages/NavBar/Weddings/Weddings.jsx
+++ src/pages/NavBar/Weddings/Weddings.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const BASE_URL = "http://localhost:3001/api/ideas";
 
 export function Weddings() {
-  return <div>Weddings</div>;
+  const [ideas, setIdeas] = useState([]);
+
+  useEffect(() => {
+    const getIdeas = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/`);
+        setIdeas(
+          response.data.filter((idea) =>
+            idea.category ? idea.category.name === "Wedding" : [...ideas]
+          )
+        );
+        console.log(ideas);
+      } catch (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      }
+    };
+
+    getIdeas();
+  }, []);
+
+  return (
+    <div>
+      <h1>Anniversary Gift Ideas</h1>
+      <ul>
+        {ideas.map((idea, _id) => (
+          <li key={_id}>
+            <h2>{idea.title}</h2>
+            <img src={idea.img} alt={idea.name} width={600} height={600} />
+            <p>{idea.description}</p>
+            <button>Share</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
